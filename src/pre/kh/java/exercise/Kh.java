9aa6b644decc162152a1455dca1792381abcf214
현서 <@DESKTOP-CGGAKO0>
2024-11-15 11:20:13 +0900

    [INIT] 24/11/15 Kh.java 첫 커밋(에제)
    1. 1~5까지의 정수 출력을 하되,
    ,을 찍게 한다.
    단, 5 뒤에는 찍지 않는다.

---
+++ src/pre/kh/java/exercise/Kh.java
@@ -0,0 +1,15 @@
+package pre.kh.java.exercise;
+
+public class Kh.java {
+	public static void main(String[]arg) {
+		for(int num = 1; num<=5; num++) {
+			if(num != 5) {
+				System.out.print(num + ",");
+			}
+			else {
+				System.out.print(num);
+			}
+		}
+	}
+
+}
---
+++ src/pre/kh/java/exercise1/Exercise_Basic.java
@@ -0,0 +1,17 @@
+package pre.kh.java.exercise1;
+
+public class Exercise_Basic {
+
+	public static void main(String[] args) {
+		for(int num = 1; num<=5; num++) {
+		if(num != 5) {
+			System.out.print(num + ",");
+		}
+		else {
+			System.out.print(num);
+		}
+	}
+
+	}
+
+}
